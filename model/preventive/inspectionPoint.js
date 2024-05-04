eaf25ba0cb3c223be336c714401eae52116a68ee
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-05-04 21:03:03 +0700

    pm1 dan ticket os 3

---
+++ model/preventive/inspectionPoint.js
@@ -0,0 +1,19 @@
+const {Sequelize} = require("sequelize")
+const db = require("../../config/database");
+
+const {DataTypes} = Sequelize
+
+const inspectionPoint = db.define(
+    "inspection_point"
+    ,{
+        inspectionPoint: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            validate:{
+               notEmpty:true
+            }
+        },
+})
+
+module.exports = inspectionPoint
+
