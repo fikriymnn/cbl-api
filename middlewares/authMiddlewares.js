c59db4455de11cb4947de6a982f27d8577abd659
acepW <acepwahyu001@gmail.com>
2024-07-15 18:29:00 +0700

    upp

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken");
 
 const authMiddlewares = {
   auth: async (req, res, next) => {
+    console.log(req.cookies.access_token);
     try {
       if (!req.cookies.access_token)
         return res.status(500).json({ msg: "pliss login" });
+
       jwt.verify(
         req.cookies.access_token,
         process.env.JWT_ACC_SECRET,
--- routes/mtc/spbServiceSparepartRoutes.js
+++ routes/mtc/spbServiceSparepartRoutes.js
@@ -30,11 +30,16 @@ router.post("/spbServiceSparepartMany", auth, createManySpbServiceSparepart);
 router.put("/spbServiceSparepart/:id", updateSpbServiceSparepart);
 router.put(
   "/spbServiceSparepartMonitoring/:id",
+  auth,
   updateMonitoringSpbServiceSparepart
 );
-router.put("/approveSpbService/:id", approveSpbServiceSparepart);
+router.put("/approveSpbService/:id", auth, approveSpbServiceSparepart);
 router.put("/tolakSpbService/:id", tolakSpbStokSparepart);
-router.put("/doneSpbServicePurchase/:id", doneSpbServiceSparepartPurchase);
+router.put(
+  "/doneSpbServicePurchase/:id",
+  auth,
+  doneSpbServiceSparepartPurchase
+);
 router.put("/verifikasiSpbServiceqQc/:id", verifikasiSpbServiceSparepartQc);
 router.put("/rejectSpbServiceqQc/:id", rejectSpbServiceSparepartQc);
 // router.delete("/spbStokSparepart/:id", deletespbStokSparepart);
