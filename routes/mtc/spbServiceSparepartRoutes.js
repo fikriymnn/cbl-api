f75823985e673a5da39d7e5c4800ada9cf7f20db
acepwahyu <acepwahyu001@gmail.com>
2024-07-16 13:07:12 +0700

    Update spbServiceSparepartRoutes.js

--- routes/mtc/spbServiceSparepartRoutes.js
+++ routes/mtc/spbServiceSparepartRoutes.js
@@ -30,14 +30,12 @@ router.post("/spbServiceSparepartMany", auth, createManySpbServiceSparepart);
 router.put("/spbServiceSparepart/:id", updateSpbServiceSparepart);
 router.put(
   "/spbServiceSparepartMonitoring/:id",
-  auth,
   updateMonitoringSpbServiceSparepart
 );
 router.put("/approveSpbService/:id", auth, approveSpbServiceSparepart);
 router.put("/tolakSpbService/:id", tolakSpbStokSparepart);
 router.put(
   "/doneSpbServicePurchase/:id",
-  auth,
   doneSpbServiceSparepartPurchase
 );
 router.put("/verifikasiSpbServiceqQc/:id", verifikasiSpbServiceSparepartQc);
