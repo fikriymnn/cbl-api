fdcdd8445ee0fd654acc53f33b878f542422adf4
acepW <acepahyu001@gmail.com>
2024-09-09 11:55:32 +0700

    update

--- routes/qc/capa/capaRoutes.js
+++ routes/qc/capa/capaRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const capa = require("../../../controller/qc/capa/capaTiketController");
 const { auth } = require("../../../middlewares/authMiddlewares");
 
-router.get("/capa/:id?", auth, capa.getCapaTicket);
-router.post("/capa/submit/:id", auth, capa.submitCapaTicket);
+router.get("/capa/:id?", capa.getCapaTicket);
+router.post("/capa/submit/:id", capa.submitCapaTicket);
 router.put("/capa/verifikasiQa/:id", auth, capa.verifikasiCapaQa);
 router.put("/capa/verifikasiMr/:id", auth, capa.verifikasiCapaMr);
 
