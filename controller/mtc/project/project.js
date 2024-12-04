80203f194ff10a6a3e7387cdde2dd77f20de3aa9
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-12-05 01:31:49 +0700

    update

---
+++ controller/mtc/project/project.js
@@ -0,0 +1,32 @@
+const projectControllers = {
+    getProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    createProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    deleteProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    updateProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    }
+}
+
+module.exports = projectControllers
---
+++ controller/mtc/project/subproject.js
@@ -0,0 +1,32 @@
+const subProjectControllers = {
+    getSubProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    createSubProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    deleteSubProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    },
+    updateSubProject : async (req,res)=>{
+        try{
+
+        }catch(err){
+            res.status(500).json({msg:err.message})
+        }
+    }
+}
+
+module.exports = subProjectControllers
