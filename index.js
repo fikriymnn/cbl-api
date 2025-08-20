a826b6ddbd07f3440b5149643c1f911c8b71a4ae
acepW <acepwahyu001@gmail.com>
2025-08-20 16:03:20 +0700

    up

--- index.js
+++ index.js
@@ -280,7 +280,7 @@ const app = express();
 
 // // model sync to table (pancingan)
 // (async () => {
-//   await MasterCustomer.sync({ alter: true });
+//   await inspeksiLemAwalPoint.sync({ alter: true });
 // })();
 
 // const ip100 = 75
